4733db55b3a6271cb3c0dba98403d0b1929a4f6f
Boofny <Brandonpizano7557@gmail.com>
2025-06-08 10:49:33 -0400

    WIP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,6 @@
 return{
       "neovim/nvim-lspconfig",
-        -- event =  {"BufReadPre", "InsertEnter"},-- Lazy load when editing
-        event = "InsertEnter",
+        event =  {"BufReadPre", "InsertEnter"},-- Lazy load when editing
       dependencies = {
         {
         "williamboman/mason.nvim",
