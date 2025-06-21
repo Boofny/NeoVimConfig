f1c0eeb2135fd1a3745a03b963e60c087e2e012d
Boofny <Brandonpizano7557@gmail.com>
2025-06-21 15:37:25 -0400

    removed all the commented parts

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
--- require("config.lazy")
-
 vim.opt.relativenumber = true -- Show relative line numbers for other lines
 vim.opt.cursorline = true
 vim.opt.clipboard = ""
@@ -135,95 +132,6 @@ require("lazy").setup({
   opts_extend = { "sources.default" }
 },
 
-
-    -- {
-    --   "hrsh7th/nvim-cmp",
-    --     event = "InsertEnter", -- ⬅ this ensures lazy loading
-    --   dependencies = {
-    --     "hrsh7th/cmp-nvim-lsp",
-    --     "hrsh7th/cmp-buffer",
-    --     "hrsh7th/cmp-path",
-    --     "L3MON4D3/LuaSnip",
-    --     "saadparwaiz1/cmp_luasnip",
-    --   },
-    --   config = function()
-    --     local cmp = require("cmp")
-    --     local luasnip = require("luasnip")
-    --     cmp.setup({
-    --       snippet = {
-    --         expand = function(args)
-    --           luasnip.lsp_expand(args.body)
-    --         end,
-    --       },
-    --       mapping = cmp.mapping.preset.insert({
-    --         ["<Tab>"] = cmp.mapping.select_next_item(),
-    --         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-    --         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    --       }),
-    --
-    --       sources = cmp.config.sources({
-    --         { name = "nvim_lsp" },
-    --         { name = "luasnip" }, -- This enables snippet completion
-    --         { name = "buffer" },
-    --         { name = "path" },
-    --       }),
-    --     })
-    --   end,
-    -- },
-
-    -- {
-    --   "nvim-lualine/lualine.nvim",
-    --   dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- },
-
-    -- {
-    --   "lukas-reineke/indent-blankline.nvim",
-    --   event = "InsertEnter",
-    --   config = function()
-    --     require("ibl").setup({
-    --       indent = { char = "│" },
-    --       scope = { enabled = true, show_start = true, show_end = true },
-    --     })
-    --   end,
-    -- },
-
-    -- {
-    --   "nvim-neo-tree/neo-tree.nvim",
-    --   branch = "v3.x",
-    --   dependencies = {
-    --     "nvim-lua/plenary.nvim",
-    --     "nvim-tree/nvim-web-devicons",
-    --     "MunifTanjim/nui.nvim",
-    --   },
-    --   keys = {
-    --     {
-    --       "<leader>e",
-    --       function()
-    --         -- Load and setup only when key is pressed
-    --         require("neo-tree").setup({
-    --             window = {
-    --             position = "right",  
-    --             },
-    --         })
-    --         require("neo-tree.command").execute({ toggle = true })
-    --       end,
-    --       desc = "Toggle Neo-tree",
-    --     },
-    --   },
-    -- },
-
-    -- {
-    --   "nvim-treesitter/nvim-treesitter",
-    --     event = { "BufReadPre", "BufNewFile" }, -- Lazy load when editing
-    --   build = ":TSUpdate",
-    --   config = function()
-    --     require("nvim-treesitter.configs").setup({
-    --       ensure_installed = { "java", "lua", "cpp", "python", "html", "css", "go" }, -- Add languages you need
-    --       highlight = { enable = true }, -- Enable syntax highlighting
-    --       indent = { enable = true }, -- Enable smart indentation
-    --     })
-    --   end,
-    -- },
   },
 
   install = { colorscheme = { "habamax" } },
@@ -235,109 +143,10 @@ require("lazy").setup({
 require('mini.surround').setup()  -- better surround handling
 require('mini.pairs').setup()     -- auto-pairing brackets
 
-
-
 require("catppuccin").setup({
   transparent_background= true,
 })
 vim.cmd.colorscheme("catppuccin")
---
--- local cmp = require("cmp") 
---
--- cmp.setup({
---   sources = {
---     { name = "nvim_lsp" }, -- LSP source
---     {
---       name = "buffer",
---       keyword_length = 1,
---       option = {
---         get_bufnrs = function()
---           -- Include all buffers in completion (including from other tabs)
---           return vim.api.nvim_list_bufs()
---         end,
---       },
---     },
---     { name = "path" }, -- File path completion
---     { name = "luasnip" }, -- Snippet completion
---   },
--- })
---
--- require("lualine").setup({
---   options = {
---     icons_enabled = true,
---     theme = "palenight",
---     component_separators = { left = "", right = "" },
---     section_separators = { left = "", right = "" },
---     disabled_filetypes = {
---       statusline = {},
---       winbar = {},
---     },
---     ignore_focus = {},
---     always_divide_middle = true,
---     always_show_tabline = true,
---     globalstatus = false,
---     refresh = {
---       statusline = 100,
---       tabline = 100,
---       winbar = 100,
---     },
---   },
---   sections = {
---     lualine_a = { {"mode", icon = "  "} },
---     lualine_b = { "branch", "diff", "diagnostics" },
---     lualine_c = { "filename" },
---     lualine_x = {  "encoding", "fileformat", "filetype" },
---     lualine_y = { "progress" },
---     lualine_z = { "location", "lsp_status" },
---   },
---   inactive_sections = {
---     lualine_a = {},
---     lualine_b = {},
---     lualine_c = { "filename" },
---     lualine_x = { "location" },
---     lualine_y = {},
---     lualine_z = {},
---   },
---   tabline = {},
---   winbar = {},
---   inactive_winbar = {},
---   extensions = {},
--- })
--- require("neo-tree").setup({
---     window = {
---         position = "right",
---     },
---   event_handlers = {
---     {
---       event = "file_opened",
---       handler = function(file_path)
---         local dir = vim.fn.fnamemodify(file_path, ":h")
---         vim.cmd("lcd " .. dir) -- Update working directory
---       end,
---     },
---   },
--- })
-
-
--- local cmp = require("cmp")
--- local luasnip = require("luasnip")
---
--- cmp.setup({
---   snippet = {
---     expand = function(args)
---       luasnip.lsp_expand(args.body)
---     end,
---   },
---   mapping = cmp.mapping.preset.insert({
---     ["<Tab>"] = cmp.mapping.confirm({ select = true }),
---     ["<C-Space>"] = cmp.mapping.complete(),
---   }),
---   sources = {
---     { name = "nvim_lsp" },
---     { name = "luasnip" },
---   },
--- })
--- vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
