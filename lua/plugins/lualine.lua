546a7c76386c2b651908dbe45d3942e5a14f72d9
Boofny <Brandonpizano7557@gmail.com>
2025-06-21 21:06:42 -0400

    WIP

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return{
         icons_enabled = true,
         theme = "palenight",
         component_separators = { left = "", right = "" },
-        component_separators= { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
         disabled_filetypes = {
           statusline = {},
