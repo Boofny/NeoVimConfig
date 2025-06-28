987366653c41810afa5bda1550adb34fbc17a401
Boofny <Brandonpizano7557@gmail.com>
2025-06-27 20:27:00 -0400

    changled lua line theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return{
     require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = "ayu_dark",
+        theme = "ayu_mirage",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
