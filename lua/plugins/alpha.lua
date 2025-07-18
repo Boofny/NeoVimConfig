617c3fbd1d686937f36ca90af0b9c0f87a9dc73a
Boofny <Brandonpizano7557@gmail.com>
2025-07-18 11:57:24 -0400

    edit alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -105,7 +105,8 @@ dashboard.section.buttons.val = {
   dashboard.button("f", "  Find File", ":lua Snacks.picker.files()<CR>"),
   dashboard.button("r", " 󰱼 Recent Files", ":lua Snacks.picker.recent()<CR>"),
   dashboard.button("n", "  New File", ":ene <BAR> startinsert<CR>"),
-  dashboard.button("c", "  Neovim Config", ":e ~/.config/nvim/init.lua<CR>"),
+  -- dashboard.button("c", "  Neovim Config", ":e ~/.config/nvim/init.lua<CR>"),
+  dashboard.button("L", "   Lazy󰒲 ", ":Lazy<CR>"),
   dashboard.button("q", " 󰩈 Quit", ":qa<CR>"),
 }
 
