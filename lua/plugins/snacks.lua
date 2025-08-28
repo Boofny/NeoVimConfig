83645cd63da5086175c3505b264e7ebda8e585e8
Boofny <Brandonpizano7557@gmail.com>
2025-08-28 09:45:23 -0400

    removed input and lazygit from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,8 +15,6 @@ return{
     bigfile = { enabled = true },
     indent = { enabled = true },
     quickfile= { enabled = true },
-    input = { enabled = true }, --try getting rid of input and lazygit
-    lazygit = { enabled = true }, --
     picker = {enabled = true, peview = nil},
     dashboard = {
       preset = {
