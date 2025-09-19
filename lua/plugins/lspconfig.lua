5971a96b1b368542d744870a65288c32d62fb047
Boofny <Brandonpizano7557@gmail.com>
2025-09-19 18:46:14 -0400

    refactored the lspconfig changes

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -29,9 +29,38 @@
 --     }
 
 
+-- return {
+--   "neovim/nvim-lspconfig",
+--   event = { "BufReadPre", "BufNewFile "}, --get rid of burNewFile
+--   dependencies = {
+--     { "williamboman/mason.nvim", lazy = true },
+--     { "williamboman/mason-lspconfig.nvim", lazy = true },
+--   },
+--   config = function()
+--     local capabilities = require("blink.cmp").get_lsp_capabilities()
+--
+--     require("mason").setup()
+--
+--     local lspconfig = require("lspconfig")
+--     local mason_lspconfig = require("mason-lspconfig")
+--
+--     require("mason-lspconfig").setup({
+--       automatic_installation = true,
+--     })
+--
+--     -- lspconfig.tsserver.setup({ --originally this was tsserver but thats old i think so ts_ls works
+--     lspconfig.ts_ls.setup({
+--       capabilities = require("blink.cmp").get_lsp_capabilities(),
+--       filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+--     })
+--   end,
+-- }
+
+
+
 return {
   "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile "}, --get rid of burNewFile
+  event = { "BufReadPre", "BufNewFile" }, -- or "BufEnter" if you prefer
   dependencies = {
     { "williamboman/mason.nvim", lazy = true },
     { "williamboman/mason-lspconfig.nvim", lazy = true },
@@ -41,18 +70,37 @@ return {
 
     require("mason").setup()
 
-    local lspconfig = require("lspconfig")
-    local mason_lspconfig = require("mason-lspconfig")
-
     require("mason-lspconfig").setup({
-      automatic_installation = true,
+      automatic_installation = false,
     })
 
-    -- lspconfig.tsserver.setup({ --originally this was tsserver but thats old i think so ts_ls works
-    lspconfig.ts_ls.setup({
-      capabilities = require("blink.cmp").get_lsp_capabilities(),
+    -- ✅ configure ts_ls
+    vim.lsp.config["ts_ls"] = {
+      capabilities = capabilities,
       filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+    }
+
+    -- ✅ start ts_ls when these filetypes are opened
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+      callback = function()
+        vim.lsp.start(vim.lsp.config["ts_ls"])
+      end,
     })
+
+    -- ✅ optional: auto-setup ALL installed servers
+    -- require("mason-lspconfig").setup_handlers({
+    --   function(server_name)
+    --     vim.lsp.config[server_name] = {
+    --       capabilities = capabilities,
+    --     }
+    --     vim.api.nvim_create_autocmd("FileType", {
+    --       pattern = "*",
+    --       callback = function()
+    --         vim.lsp.start(vim.lsp.config[server_name])
+    --       end,
+    --     })
+    --   end,
+    -- })
   end,
 }
-
