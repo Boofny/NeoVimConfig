aefb3355d3d57b207c5b2c9b352846933ed5b1bf
Boofny <Brandonpizano7557@gmail.com>
2026-04-27 13:03:24 -0400

    update on tree sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,19 +16,40 @@
 
 -- This is mainly used for highlighting code and some indentation
 -- some languages pre installed but more can be installed later 
-return{
+-- return{
+--   "nvim-treesitter/nvim-treesitter",
+--   -- event = { "BufReadPost", "BufNewFile" },
+--   lazy = false,
+--   build = ":TSUpdate",
+--   config = function()
+--     require("nvim-treesitter").setup({
+--       ensure_installed = { "java", "lua", "cpp", "python", "html", "css", "go" }, -- Add languages you need
+--       highlight = {
+--         enable = true,
+--         additional_vim_regex_highlighting = false, -- avoids double-highlighting
+--       },
+--       indent = { enable = false}, -- Enable smart indentation
+--     })
+--   end,
+-- }
+
+return {
   "nvim-treesitter/nvim-treesitter",
-  -- event = { "BufReadPost", "BufNewFile" },
   lazy = false,
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter").setup({
-      ensure_installed = { "java", "lua", "cpp", "python", "html", "css", "go" }, -- Add languages you need
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false, -- avoids double-highlighting
+      ensure_installed = {
+        "java", "lua", "cpp", "python", "html", "css", "go",
+        "typescript", "tsx", "javascript", -- add these
       },
-      indent = { enable = false}, -- Enable smart indentation
+    })
+
+    -- New main-branch API: enable highlighting via autocommand
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
