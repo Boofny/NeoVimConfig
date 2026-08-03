dace1ec7d3453376b7f7047aeadc2b4ce0e04b76
Boofny <Brandonpizano7557@gmail.com>
2026-08-03 12:38:27 -0400

    added teleport listing to the snacks dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -96,7 +96,7 @@ return{
           -- { icon = " ", key = "q", desc = "Quit", action = ":qa" },
          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-         { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+         { icon = " ", key = "t", desc = "Teleport Files", action = ":ListMarkFiles" },
          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
