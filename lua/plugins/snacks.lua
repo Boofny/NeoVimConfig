9eb3d32f83c961c7f4f788071bbc06b38dbf050c
Boofny <Brandonpizano7557@gmail.com>
2026-08-03 14:11:25 -0400

    using the fzf list or teleport marks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -96,7 +96,7 @@ return{
           -- { icon = " ", key = "q", desc = "Quit", action = ":qa" },
          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-         { icon = " ", key = "t", desc = "Teleport Files", action = ":ListMarkFiles" },
+         { icon = " ", key = "t", desc = "Teleport Files", action = ":FindMarks" },
          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
